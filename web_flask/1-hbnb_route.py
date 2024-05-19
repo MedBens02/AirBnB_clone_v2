32eee9585fc606bdda60d113165e6f1e76adadfe
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-05-18 21:46:19 -0700

    fix pucodeStyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,10 +16,12 @@ def hello_hbnb():
     '''Prints "Hello HBNB!" '''
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''Prints "HBNB" '''
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
