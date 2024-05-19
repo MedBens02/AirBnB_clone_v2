5ae1dc267e8420f7529b500e2284d05dc49c4347
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-05-18 22:06:14 -0700

    add new route that gets an int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+    Listen : on 0.0.0.0 port 5000
+    Routes :
+        /              : display "Hello HBNB!"
+        /hbnb          : display "HBNB"
+        /c/<text>      : display "C ", followed by the value of the
+                    text variable (replace underscores with spaces)
+        /python/<text> : display "Python", followed by the value of
+                    the text variable
+                    The default value of text is "is cool"
+        /number/<n>: display "n is a number" only if n is an integer
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Prints "Hello HBNB!" '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Prints "HBNB" '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    '''Prints "C " followed by the value of the text variable
+            with underscores replaced by spaces'''
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    '''Prints "Python " followed by the value of the text variable
+            with underscores replaced by spaces
+        Default text: "is cool"
+    '''
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Prints "n is a number" only if n is an integer'''
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
